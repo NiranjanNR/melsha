3a5b682bd77b7e76b59c9da57e724eaddb9f7c87
NiranjanNR <kidkool8@gmail.com>
2024-08-10 20:51:54 +0530

    bug

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const LandingPage = () => {
     return (
-        <div>LandingPage</div>
+        <div className='text-4xl'>
+            gasd
+        </div>
     )
 }
 
