c8a7adbf35a8f45d986ddfc18356e0452df93d41
NiranjanNR <kidkool8@gmail.com>
2024-08-11 00:33:21 +0530

    landing complete

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import './LandingPage.css'
+import './LandingPage.css';
+import home from '../../Assets/Home.png';
+import thums from '../../Assets/Thumbs up.png';
 
 const LandingPage = () => {
     return (
@@ -21,8 +23,29 @@ const LandingPage = () => {
                     </div>
                 </div>
             </div>
-            <div className='bg-[#011428] ml-auto mr-[150px] mt-[-90px] w-[700px] h-[180px]'>
-                ads
+            <div className='bg-[#011428] ml-auto mr-[100px] mt-[-90px] w-[750px] h-[180px] flex items-center px-10'>
+                <div className='w-[50%] h-[100%] flex items-center  text-[#cecece] mr-8'>
+                    <img src={home} className='h-[50px] opacity-90 mb-4' />
+                    <div className='ml-8'>
+                        <div className='text-[20px] tracking-[1.1px] font-semibold mb-[10px]'>
+                            24/7 Services
+                        </div>
+                        <div className='mb-2 '>
+                            We try our best to bring the customer the greatest service, so, you can place your trust
+                        </div>
+                    </div>
+                </div>
+                <div className='w-[50%] h-[100%] flex items-center  text-[#cecece]'>
+                    <img src={thums} className='h-[50px] opacity-90 mb-4' />
+                    <div className='ml-8'>
+                        <div className='text-[20px] tracking-[1.1px] font-semibold mb-[10px]'>
+                            High Quality
+                        </div>
+                        <div className='mb-2 '>
+                            Melsha provides the high quality service, you don't need to worry about the result
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import logo from '../../Assets/Logo.png';
+import './NavBar.css';
 
 const NavBar = () => {
     return (
-        <div className='h-[10vh] w-full'>
+        <div className='h-[10vh] w-[100vw] flex items-center '>
             <div>
-                <img src={logo} className='h-[80px]' />
+                <img src={logo} className='h-[62px] mb-2 ml-4' />
+            </div>
+            <div className='flex ml-auto font-semibold mt-2 mr-10'>
+                <div className=''>
+                    <button class="cta">
+                        <span class="hover-underline-animation">Home</span>
+                    </button>
+                </div>
+                <div className='ml-10'>
+                    <div className=''>
+                        <button class="cta">
+                            <span class="hover-underline-animation">About</span>
+                        </button>
+                    </div>
+                </div>
+                <div className='ml-10'>
+                    <div className=''>
+                        <button class="cta">
+                            <span class="hover-underline-animation">Services</span>
+                        </button>
+                    </div>
+                </div>
+                <div className='ml-10'>
+                    <div className=''>
+                        <button class="cta">
+                            <span class="hover-underline-animation">Blogs</span>
+                        </button>
+                    </div>
+                </div>
+                <div className='ml-10 mr-4'>
+                    <div className=''>
+                        <button class="cta">
+                            <span class="hover-underline-animation">Contacts</span>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     )
